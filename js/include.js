6bee305f4af4457c843bb65e89c302e10f067831
Jonathan Hill <jonathan@jh.codes>
2024-12-31 19:22:32 -0800

    Move sidebar to separate file

---
+++ js/include.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch("/components/sidebar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector("nav").innerHTML = data;
+        });
+
+    // fetch("/components/footer.html")
+    //     .then(response => response.text())
+    //     .then(data => {
+    //         document.querySelector("footer").innerHTML = data;
+    //     });
+});
