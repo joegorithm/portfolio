18ad97ed9afd15583cb2fa9d2921863d702b63a4
Jonathan Hill <jonathan@jh.codes>
2025-01-12 16:52:59 -0800

    Create form and link to Google App Script API and Google Sheets

---
+++ js/contact-form.js
@@ -0,0 +1,14 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzbrJLUEaCczT-NnzmDWJ8rfpkWsZW46_oYDsmoBkIFGMmvn65kd06ThPbmWiU0OpkL/exec'
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+    alert('Form submitted successfully!');
+        form.reset(); // Clear the form after submission
+    })
+    .catch(error => {
+        alert('Failed to submit form. Please try again.');
+    });
+})
