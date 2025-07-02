8cd8961792fdd8d5d1215f9faeb4fe750c94aaf1
Jonathan Hill <jonathan@jh.codes>
2025-07-02 10:23:25 -0700

    Make border radius of anchor tags same as element inside

--- js/technologies.js
+++ js/technologies.js
@@ -111,7 +111,7 @@ function renderTechnologies() {
         for (var i = 0; i < technology.length; i++) {
             const tech = technologies.find(t => t.id === technology[i].toLowerCase());
             if (tech) {
-                element.innerHTML += `<a href="/search?tool=${tech.id}"><div class="tool-tag ${tech.class}">${tech.svg}<span>${tech.name}</span></div></a>`;
+                element.innerHTML += `<a class="tool-tag-link" href="/search?tool=${tech.id}"><div class="tool-tag ${tech.class}">${tech.svg}<span>${tech.name}</span></div></a>`;
             } else {
                 element.innerHTML += `<p style="color: var(--form-input-placeholder-color); font-size: 1rem; font-weight: 200;">No associated technologies or tools</p>`;
             }
