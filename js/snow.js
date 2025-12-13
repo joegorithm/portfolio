facb89f1db6330ff704d7a0af169e654e3b8764b
Jonathan Hill <jonathan@jh.codes>
2025-12-13 08:04:24 -0800

    Fix snow effect activation date to start from December 19

--- js/snow.js
+++ js/snow.js
@@ -1,8 +1,8 @@
 // If the current date is during the week leading up to Christmas, let it snow, let it snow, let it snow!
 
 var sf;
-// Change the 1 to 19 !!!
-if (new Date().getMonth() === 11 && (new Date().getDate() >= 1 && new Date().getDate() <= 25)) {
+
+if (new Date().getMonth() === 11 && (new Date().getDate() >= 19 && new Date().getDate() <= 25)) {
     sf = new Snowflakes({
         color: "#a7c9d3",
         count: 100,
